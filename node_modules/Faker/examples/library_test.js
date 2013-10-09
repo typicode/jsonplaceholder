eac6835774cd8a8439516ab4878f38dc563e05ed
typicode <typicode@gmail.com>
2013-10-09 07:29:27 +0200

    Remove

--- node_modules/Faker/examples/library_test.js
+++
@@ -1,9 +0,0 @@
-var util = require('util');
-
-var definitions = require('../lib/definitions');
-
-var Faker = require('../index');
-
-var card = Faker.Helpers.createCard();
-
-util.puts(JSON.stringify(card));
--- node_modules/Faker/examples/node_min_test.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-var util = require('util');
-
-var Faker = require('../Faker');
-
-var card = Faker.Helpers.createCard();
-
-util.puts(JSON.stringify(card));
