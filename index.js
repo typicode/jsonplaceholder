d3e2d235f9a99b1bf7c4131054f7474cc134ceca
Typicode <typicode@gmail.com>
2014-05-09 08:17:58 +0200

    Use JSON-Server 0.3.3

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-var server = require('json-server'),
-	  seed = require('./seed.js');
+var fs     = require('fs')
+var server = require('json-server')
 
-var db = seed.run();
+var port = 3000 || process.env.PORT
 
-server.run(db, { mode: 'read-only' });
+function reset() {
+  var data = JSON.parse(fs.readFileSync('data.json', 'utf-8'))
+  server.low.db = data
+}
+
+server.low.on('update', reset)
+server.low.on('remove', reset)
+server.low.on('add', reset)
+
+server.listen(port, function() {
+  console.log('JSONPlaceholder listening on http://localhost:' + port)
+  reset()
+})
