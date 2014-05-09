14472159626af573e2049b948ae509fa6608f69d
Typicode <typicode@gmail.com>
2014-05-09 09:17:59 +0200

    Use lowdb load function

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-var fs     = require('fs')
 var server = require('json-server')
 
 var port = 3000 || process.env.PORT
 
 function reset() {
-  var data = JSON.parse(fs.readFileSync('data.json', 'utf-8'))
-  server.low.db = data
+  server.low.load('data.json')
 }
 
 server.low.on('update', reset)
