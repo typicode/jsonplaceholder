5885b0a63da419969fa93c49f32b01de94c38dc5
Typicode <typicode@gmail.com>
2015-02-04 06:48:06 +0100

    Update package and code to json-server 0.5.12 WIP

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 var server = require('json-server')
+var data = require('./data.json')
 
+// TODO make sure data can't be changed
+
+// Get port
 var port = process.env.PORT || 3000 
 
-function reset() {
-  server.low.load(__dirname + '/data.json')
-}
-
-server.low.autoSave = false
-server.low.on('change', reset)
-
-server.listen(port, function() {
+// Start server
+server(data).listen(port, function() {
   console.log('JSONPlaceholder listening on http://localhost:' + port)
-  reset()
 })
