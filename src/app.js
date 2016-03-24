054e90a9c3e8c56d4782f1ccd075e6e031b54795
Typicode <typicode@gmail.com>
2016-03-24 01:37:57 +0100

    Update to Node v5 and add tests

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const jsonServer = require('json-server')
+const clone = require('clone')
+const data = require('../data.json')
+
+const app = jsonServer.create()
+const router = jsonServer.router(clone(data))
+
+app.use((req, res, next) => {
+  if (req.path === '/') return next()
+  router.db.object = clone(data)
+  next()
+})
+
+app.use(jsonServer.defaults())
+app.use(router)
+
+module.exports = app
---
+++ test/index.js
@@ -0,0 +1,26 @@
+const test = require('tape')
+const request = require('supertest')
+const app = require('../src/app')
+
+test('GET /', (t) => {
+  request(app)
+    .get('/')
+    .expect(200, (err) => t.end(err))
+})
+
+test('POST /', (t) => {
+  request(app)
+    .post('/posts')
+    .send({ body: 'foo' })
+    .expect(201, (err) => {
+      t.error(err)
+      // Check that GET /posts length still returns 100 items
+      request(app)
+        .get('/posts')
+        .expect(200, (err, res) => {
+          t.error(err)
+          t.equal(res.body.length, 100, 'more than 100 posts found')
+          t.end()
+        })
+    })
+})
